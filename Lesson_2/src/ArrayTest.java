1fde8dcadb7d9bf23293ded10b0f1e8769228b70
Oleksii <lamrak@gmail.com>
2014-11-24 17:32:31 +0200

    add lesson 2 and gitignore

---
+++ Lesson_2/src/ArrayTest.java
@@ -0,0 +1,21 @@
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		
+		//init
+		System.out.println("Init array");
+		int j = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = 100 + j;
+		}
+		
+		//print 
+		System.out.println("Print array");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
